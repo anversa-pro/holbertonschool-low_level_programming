bbdbcddc114c176daee665552fed93d8a844bacf
anversa-pro <angelavergaras@gmail.com>
2021-06-17 20:34:15 -0700

    [MODIFY] 6- add odder printers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,5 +15,9 @@ int main(void)
 	float varfloat;
 	/*printing vars*/
 	printf("Size of a char: %d byte(s)\n", sizeof(varchar));
+	printf("Size of an int: %d byte(s)\n", sizeof(varint));
+	printf("Size of a long int: %d byte(s)\n", sizeof(varlong));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(varlongl));
+	printf("Size of a float: %d byte(s)\n", sizeof(varfloat));
 	return (0);
 }
