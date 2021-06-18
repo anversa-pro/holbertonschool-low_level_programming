85a900e4a59a43681c20c899b6bcfd0ca53414ff
anversa-pro <angelavergaras@gmail.com>
2021-06-18 15:35:36 -0700

    [MODIFY] 4- add while and if function

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,5 +7,16 @@
 
 int main(void)
 {
+	int letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar (letter);
+			letter++;
+		}
+	}
+	putchar ('\n');
 	return (0);
 }
