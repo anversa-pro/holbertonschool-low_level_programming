860f32406693128ef5059e0188f4f5858aac2395
anversa-pro <angelavergaras@gmail.com>
2021-06-18 16:51:07 -0700

    [MODIFY] 100- create function

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,5 +7,21 @@
 
 int main(void)
 {
+	int digit10 = '0';
+	int digit1;
+	int number;
+
+	while (digit10 <= '9')
+	{
+		digit1 = digit10 + '1';
+		while (digit1 <= '9')
+		{
+			number = (digit10 * 10) + digit1;
+			putchar (number);
+			digit1++;
+		}
+		digit10++;
+	}
+	putchar ('\n');
 	return (0);
 }
