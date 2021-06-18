3b434215af2467590e4eed6ed3f29a3ffb6b12ba
anversa-pro <angelavergaras@gmail.com>
2021-06-17 19:59:10 -0700

    [CREATE]6-  declaration and print

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	/*var declaration*/
+	char varchar;
+	int varint;
+	long varlong;
+	long long varlongl;
+	float varfloat;
+	/*printing vars*/
+	puts("Size of a char: %d byte(s)\n", sizeof(varchar));
+	return (0);
+}
