d0fe7fbdb47e2ea3dd9ee7c563a105f7a2a5f635
anversa-pro <angelavergaras@gmail.com>
2021-06-18 21:24:15 -0700

    [MODIFY] 101- Create function

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,5 +7,34 @@
 
 int main(void)
 {
+	int digit100 = '0';
+	int digit10 = '0';
+	int digit1 = '0';
+
+	while (digit100 <= '9')
+	{
+		digit10++;
+		while (digit10 <= '9')
+		{
+			digit1++;
+			while (digit1 <= '9')
+			{
+				putchar (digit100);
+				putchar (digit10);
+				putchar (digit1);
+				if (digit100 != '7')
+				{
+					putchar (',');
+					putchar (' ');
+				}
+				digit1++;
+			}
+			digit10++;
+			digit1 = digit10;
+		}
+		digit100++;
+		digit10 = digit100;
+	}
+	putchar ('\n');
 	return (0);
 }
