4557f7c2d8a61725bf37222bf92a4f0f923354bc
anversa-pro <angelavergaras@gmail.com>
2021-06-18 20:47:42 -0700

    [MODIFY] 100- add an if

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,19 @@ int main(void)
 {
 	int digit10 = '0';
 	int digit1;
-	int number;
 
 	while (digit10 <= '9')
 	{
-		digit1 = digit10 + 1;
+		digit1 = digit10 + '1';
 		while (digit1 <= '9')
 		{
-			number = (digit10 * 10) + digit1;
-			putchar (number);
+			putchar (digit10);
+			putchar (digit1);
+			if (digit10 != '8')
+			{
+				putchar (',');
+				putchar (' ');
+			}
 			digit1++;
 		}
 		digit10++;
