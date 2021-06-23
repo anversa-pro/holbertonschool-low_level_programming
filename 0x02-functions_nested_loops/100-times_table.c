cdeb70c01bbad208dc546e9600497a616f562247
anversa-pro <angelavergaras@gmail.com>
2021-06-23 10:33:27 -0700

    [MODIFY] Add functions and loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,61 @@
 #include "holberton.h"
 
 /**
- * main - Define which is the purpouse
- *
- *Return: Always 0 (Success)
+ * print_table_numbers - prints comma, spaces and the number
+ * @n: input parameter to print
  */
 
-int main(void)
+void print_table_numbers(int n)
 {
-	return (0);
+	putchar(',');
+	if (n < 9)
+	{
+		_putchar(32);
+		_putchar(32);
+		_putchar(32);
+		_putchar(n + '0');
+
+	}
+	else if (n > 9 && n < 100)
+	{
+		_putchar(32);
+		_putchar(32);
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n > 99 && n < 1000)
+	{
+		_putchar(32);
+		_putchar(n / 100 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+}
+/**
+ * print_times_table - prints the n times table
+ * @n: input parameter
+ *
+ */
+
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int aux;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			j = 1;
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+			{
+				aux = i * j;
+				print_table_numbers(aux);
+				if (j == n)
+					_putchar('\n');
+			}
+		}
+	}
 }
