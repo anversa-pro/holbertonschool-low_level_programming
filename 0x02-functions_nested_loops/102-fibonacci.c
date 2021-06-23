d4c3ce23ea69ba91cb33913cffe27e3f0743cc53
anversa-pro <angelavergaras@gmail.com>
2021-06-23 15:02:27 -0700

    [MODIFY] Add main function and logical asignments

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,12 +1,30 @@
 #include "holberton.h"
 
 /**
- * main - Define which is the purpouse
+ * main - Entry point that prints the first 50 Fibonacci numbers
  *
  *Return: Always 0 (Success)
  */
 
 int main(void)
 {
+	int i;
+	int ant = 0;
+	int new = 0;
+	int sum = 1;
+
+	for (i = 0; i < 50; i++)
+	{
+		new = sum + ant;
+		printf("%d \n", new);
+		ant = sum;
+		sum = new;
+		if (i < 49)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 	return (0);
 }
