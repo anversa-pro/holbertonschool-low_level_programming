05f104adc33fd69189f77ccb23c006a3b65a1ad4
anversa-pro <angelavergaras@gmail.com>
2021-06-23 15:25:45 -0700

    [MODIFY] add function and conditionals

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,12 +1,29 @@
 #include "holberton.h"
 
 /**
- * main - Define which is the purpouse
+ * main - Entry point that prints the first 50 Fibonacci numbers
  *
  *Return: Always 0 (Success)
  */
 
 int main(void)
 {
+	int i;
+	long ant = 0;
+	long new = 0;
+	long sum = 1;
+	long even = 0;
+
+	for (i = 1; i <= 35; i++)
+	{
+		new = sum + ant;
+		printf("%ld", new);
+		ant = sum;
+		sum = new;
+		if ((i % 2) != 0 && new < 4000000)
+			even = even + new;
+	}
+	printf("%ld", even)
+	putchar('\n');
 	return (0);
 }
