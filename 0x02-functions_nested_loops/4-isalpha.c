88dfe4ebc2b5c50116ceba0e666dbb58d05f15e0
anversa-pro <angelavergaras@gmail.com>
2021-06-22 18:55:57 -0700

    [MODIFY] add function and conditionals

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,21 @@
 #include "holberton.h"
 
 /**
- * main - Define which is the purpouse
- *
- *Return: Always 0 (Success)
+ * _isalpha - Checks for alphabetic character
+ * @c: input prameter to evaluate
+ *Return: 1 if true, 0 otherwise
  */
 
-int main(void)
+int m_isalpha(int c)
 {
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
 	return (0);
 }
