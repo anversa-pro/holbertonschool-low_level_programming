0f190a5012f00c34d5026b14ab462083930a81b0
anversa-pro <angelavergaras@gmail.com>
2021-06-22 23:01:19 -0700

    [MODIFY] add functions, loops and conditionals

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,49 @@
 #include "holberton.h"
 
 /**
- * main - Define which is the purpouse
- *
- *Return: Always 0 (Success)
+ * print_to_98 - function that prints natural numbers from n to 98
+ * @n: input parameter
+ *Return: empty
  */
-
-int main(void)
+void print_to_98(int n)
 {
-	return (0);
+	int aux;
+	int temp;
+
+	if (n <= 98)
+	{
+		for (aux = n; aux <= 98; aux++)
+		{
+			if (aux >= 0)
+			{
+				print_two_digits(aux);
+			}
+			else
+			{
+				temp = aux * -1;
+				_putchar('-');
+				print_two_digits(temp);
+			}
+			_putchar(',');
+			_putchar(32);
+		}
+	}
+	else
+	{
+		for (aux = n; aux >= 98; aux--)
+		{
+			if (aux > 99)
+			{
+				_putchar(aux / 100 + '0');
+				_putchar((aux / 10) % 10 + '0');
+				_putchar(aux % 10 + '0');
+			}
+			else
+			{
+				print_two_digits(aux);
+			}
+			_putchar(',');
+			_putchar(32);
+		}
+	}
 }
