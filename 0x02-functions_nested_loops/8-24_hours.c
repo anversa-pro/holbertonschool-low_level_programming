e67f85f648b4beaec00279beaea1bd3565437118
anversa-pro <angelavergaras@gmail.com>
2021-06-22 20:35:26 -0700

    [MODIFY] parameter reseting

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,13 +13,15 @@ void jack_bauer(void)
 
 	for (i = 0; i < 24; i++)
 	{
+		j = 0;
 		for (j = 0; j <= 59; j++)
 		{
 			_putchar (i / 10 + '0');
 			_putchar (i % 10 + '0');
-			_putchar (';');
+			_putchar (':');
 			_putchar (j / 10 + '0');
 			_putchar (j % 10 + '0');
+			_putchar ('\n');
 		}
 	}
 }
