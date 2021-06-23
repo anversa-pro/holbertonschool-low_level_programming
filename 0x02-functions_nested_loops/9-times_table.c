7927f8db6e2990e7e6443d2e85f11629cd9dc985
anversa-pro <angelavergaras@gmail.com>
2021-06-22 21:44:36 -0700

    [MODIFY] change putchar and conditionals

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,32 +13,27 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		j = 0;
-		for (j = 0; j <= 9; j++)
+		j = 1;
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
 		{
 			aux = i * j;
 			if (aux > 9)
 			{
+				_putchar(',');
+				_putchar(32);
 				_putchar(aux / 10 + '0');
 				_putchar(aux % 10 + '0');
-				if (j != 9)
-				{
-					_putchar(',');
-					_putchar(32);
-				}
-				else
+				if (j == 9)
 					_putchar('\n');
 			}
 			else if (aux <= 9)
 			{
-				_putchar(-aux + '0');
-				if (j != 9)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-				}
-				else
+				_putchar(',');
+				_putchar(32);
+				_putchar(32);
+				_putchar(aux + '0');
+				if (j == 9)
 					_putchar('\n');
 			}
 		}
