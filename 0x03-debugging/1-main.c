13590be5461c9915caf5f40b9df8099452516042
anversa-pro <angelavergaras@gmail.com>
2021-06-24 13:19:23 -0700

    [MODIFY] change comment format

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int i;
 
-	printf("Infinite loop incoming : (\n)");
+	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	while (i < 0) /* This lines causes an infinite loop, i is always 0*/
-	{
-		putchar (i);
-	}
-
+/**
+*	while (i < 0)
+*	{
+*		putchar (i);
+*	}
+*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
