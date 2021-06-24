e158484c3297d06b5f67b3f231ae3169e82f1d51
anversa-pro <angelavergaras@gmail.com>
2021-06-24 15:05:10 -0700

    [MODIFY] Change comparison

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -6,4 +6,7 @@
 /*Prints whether the number stored in a variable is positive or negative*/
 void positive_or_negative(int i);
 
+/*Returns the largest of 3 numbers*/
+int largest_number(int a, int b, int c);
+
 #endif /* HOLBERTON_H */
