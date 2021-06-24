140be914fb5354db9d8da7f9426678e1cdef5098
anversa-pro <angelavergaras@gmail.com>
2021-06-23 19:02:22 -0700

    [MODIFY] Add function and conditionals

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,12 +1,30 @@
 #include "holberton.h"
 
 /**
- * main - Define which is the purpouse
+ * main - Entry point that prints the first 98 Fibonacci numbers
  *
  *Return: Always 0 (Success)
  */
 
 int main(void)
 {
+	int i;
+	long ant = 0;
+	long new = 0;
+	long sum = 1;
+
+	for (i = 0; i < 98; i++)
+	{
+		new = sum + ant;
+		printf("%ld", new);
+		ant = sum;
+		sum = new;
+		if (i < 49)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 	return (0);
 }
