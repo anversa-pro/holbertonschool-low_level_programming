35a7cb06b145abef09ecbc445f1e251b70666968
anversa-pro <angelavergaras@gmail.com>
2021-06-24 12:47:48 -0700

    [MODIFY] add code and fix

---
+++ 0x03-debugging/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+/*Prints whether the number stored in a variable is positive or negative*/
+void positive_or_negative(int i);
+
+#endif /* HOLBERTON_H */
