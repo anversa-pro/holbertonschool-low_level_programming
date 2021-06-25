ebb1816814b4299271cb59d7aedb7b85f61536d6
anversa-pro <angelavergaras@gmail.com>
2021-06-25 10:35:48 -0700

    [MODIFY] Add conditionals and description

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 
 /**
- * main - Entry point
+ * _isupper - Checks for uppercase character.
  * @c: Input parameter
- * Return: 0 always
+ * Return: 1 if true 0 false
  */
+
 int _isupper(int c)
 {
-	return (0);
+	if (c >= 65 && c <= 95)
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
