1c5e94071b7dc8b4789eb62ff4aa6b21ba12d967
anversa-pro <angelavergaras@gmail.com>
2021-06-24 21:53:06 -0700

    [CREATE] Header file

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _isupper(int c); /* checks uppercase */
+
+int _isdigit(int c); /* checks digit */
+
+int mul(int a, int b); /* multiplies tow integers */
+
+void print_numbers(void); /*prints the numbers from 0 to 9 */
+
+#endif /* HOLBERTON_H */
