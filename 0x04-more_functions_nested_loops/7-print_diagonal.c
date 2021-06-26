e61c25429c3aa640deff6c68d6c269ba2c19c9d8
anversa-pro <angelavergaras@gmail.com>
2021-06-25 22:00:22 -0700

    [CREATE] 7- function file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Draws a diagonal line
+ * @n: input number to set number of lines
+ * Return: empty
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
