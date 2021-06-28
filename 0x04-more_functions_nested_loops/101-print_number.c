8e76139619db24c96e123ec0b4ee686e1a4a40d0
anversa-pro <angelavergaras@gmail.com>
2021-06-27 23:01:59 -0700

    [MODIFY] add conditionals por MIN and MAX

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,24 +8,39 @@
 
 void print_number(int n)
 {
-	int nn = 0, digits = 1, rev = 0;
+	int nn = 0, digits = 1, rev = 0, min = 0;
 
 	if (n < 0)
 	{
 		nn = 1;
 		n = n * -1;
+		if (n == -2147483648)
+		{
+			n = (n % 1000000000) * -1;
+			min = 2;
+		}
 	}
 	while (n / 10 != 0)
 	{
-		rev = (rev + (n % 10)) * 10;
+		rev = rev + (n % 10);
+		if (digits < 9)
+			rev = rev * 10;
 		n = n / 10;
 		digits++;
 	}
-	rev = rev + n;
+	if (digits < 10)
+		rev = rev + n;
 	if (nn == 1)
 		_putchar('-');
+	if (min == 2)
+		_putchar(min + '0');
 	for (; digits > 0; digits--)
 	{
+		if (digits == 10)
+		{
+			_putchar(n + '0');
+			digits--;
+		}
 		_putchar(rev % 10 + '0');
 		rev = rev / 10;
 	}
