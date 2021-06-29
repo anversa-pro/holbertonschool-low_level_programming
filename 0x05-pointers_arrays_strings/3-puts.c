d78614e73e78a6a781923f9dec00aeb9a1b3c688
anversa-pro <angelavergaras@gmail.com>
2021-06-29 14:15:09 -0700

    [MODIFY] create conditionals

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,4 +8,12 @@
 
 void _puts(char *str)
 {
+	int s = 0;
+
+	while (str[s + 1] != '\0')
+	{
+		_putchar(str[s]);
+		s++;
+	}
+	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 /* Prints a character */
 int _putchar(char c);
