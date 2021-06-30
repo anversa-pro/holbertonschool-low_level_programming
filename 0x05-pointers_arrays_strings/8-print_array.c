8c58129798f991568067ef33f1c4c91bf39410d1
anversa-pro <angelavergaras@gmail.com>
2021-06-29 20:41:31 -0700

    [MODIFY] add conditionals and loops

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,4 @@
-#include "holberotn.h"
+#include "holberton.h"
 
 /**
  * print_array - Prints n elements of an array of integers
@@ -9,4 +9,13 @@
 
 void print_array(int *a, int n)
 {
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n-1))
+			printf(", ");
+	}
+		printf("\n");
 }
