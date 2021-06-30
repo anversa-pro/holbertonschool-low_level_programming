856de48e41f7cb03523c418a748ce253670b04b0
anversa-pro <angelavergaras@gmail.com>
2021-06-29 17:18:26 -0700

    [MODIFY] Add conditionals

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,24 @@
 #include "holberton.h"
 
 /**
- * rev_string - function that reverses a string
- * @s: input pointer to a string for reverse
+ * rev_string - Function that reverses a string
+ * @s: input string to print
  * Return: empty
  */
 
 void rev_string(char *s)
 {
+	int i = 0, j = 0;
+	char temp;
+
+	while (s[i] != '\0')
+		i++;
+	while (i > j)
+	{
+		i--;
+		temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
+		j++;
+	}
 }
