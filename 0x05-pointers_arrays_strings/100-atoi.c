e10f3cf735406a2778b67b23623672244e012c60
anversa-pro <angelavergaras@gmail.com>
2021-06-30 20:28:13 -0700

    [MODIFY] add function and conditionals

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,5 +8,19 @@
 
 int _atoi(char *s)
 {
-	return (0);
+	int i = 1, j = 0, k = 0;
+
+	while (s[k] != '\0')
+	{
+		if (s[k] == '-')
+			i = i * -1;
+		else if (s[k] >= '0' && s[k] <= '9')
+		{
+			j = (j * 10) + (s[k] - '0');
+		}
+		else if (j > 0)
+			break;
+		k++;
+	}
+	return (i * j);
 }
