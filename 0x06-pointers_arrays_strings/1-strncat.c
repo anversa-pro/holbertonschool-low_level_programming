c5877a30c81caa64bae05b319822944b7a722f39
anversa-pro <angelavergaras@gmail.com>
2021-07-02 18:18:49 -0700

    [CREATE] file and function

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strncat - concatenates two strings
+ * *@dest: input array to add content
+ * *@src: input array to append
+ * @n: bytes from src to copy
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0, k = 0;
+
+	while (src[i] != '\0')
+		i++;
+	while (dest[j] != '\0')
+		j++;
+	while (k < i && k < n)
+	{
+		dest[j] = src[k];
+		k++;
+		j++;
+	}
+	return (dest);
+}
