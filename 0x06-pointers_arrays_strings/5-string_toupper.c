1a5c8e567f070e37a070facfa6bd32ad5d30f1af
anversa-pro <angelavergaras@gmail.com>
2021-07-02 20:28:37 -0700

    [CREATE] file and function

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * *string_toupper - changes lowercase letters to uppercase
+ * *@a: input array to modify
+ * Return: pointer
+ */
+
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+			a[i] = a[i] - 32;
+		i++;
+	}
+	return a;
+}
