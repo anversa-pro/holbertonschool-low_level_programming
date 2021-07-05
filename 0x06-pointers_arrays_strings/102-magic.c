a7d19ffe78ee11ac5f4c5181b9b025eb0dbbfb9b
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-05 14:05:56 -0500

    [MODIFY] line position

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -18,7 +18,6 @@ int main(void)
    * - you are not allowed to code anything else than this line of code
    */
   /* ...so that this prints 98\n */
-  *(p + 5) = 98;
   printf("%d\n", *p);
   printf("a[2] = %d\n", a[2]);
   return (0);
