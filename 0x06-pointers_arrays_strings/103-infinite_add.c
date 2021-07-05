61ac7eea4ef59fc4b47c4b5ee6f6180ce053f9e8
anversa-pro <angelavergaras@gmail.com>
2021-07-05 14:36:08 -0700

    [CREATE] advanced files

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * *infinite_add - adds two numbers
+ * *@n1: input number 1
+ * *@n2: input number 2
+ * *@r: buffer to store de addition
+ * @size_r: buffer size
+ * Return: 0 If the result can not be stored or pointer to the result
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,13 @@
+include "holberton.h"
+
+/**
+ * print_buffer - prints a buffer, 10 bytes per line.
+ * *@b: pointer to the buffer
+ * @size: buffer to print if it is greater then zero.
+ * Return: Empty
+ */
+
+void print_buffer(char *b, int size)
+{
+	printf("\n");
+}
