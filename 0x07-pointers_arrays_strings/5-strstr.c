4fb0dfae6f2bef097e4a3b996b3364958b9e3298
anversa-pro <angelavergaras@gmail.com>
2021-07-06 16:34:10 -0700

    [MODIFY] function and conditionals

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,13 +1,24 @@
 #include "holberton.h"
 
 /**
- * *_strstr - Locates a substring
+ * *_strstr - locates a substring
  * *@haystack: pointer to a string
  * *@needle: pointer to a substring
- * Return: a pointer to the first occurrence or null
+ * Return: a pointer to the first occurrence
  */
 
 char *_strstr(char *haystack, char *needle)
 {
+	int i;
+	int j;
+
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (needle[i] == haystack[j])
+				return (&haystack[j]);
+		}
+	}
 	return (NULL);
 }
