4fcc61732df5b263402e5e6fc7fcc130588c32df
anversa-pro <angelavergaras@gmail.com>
2021-07-05 21:07:32 -0700

    [MODIFY] change conditionals

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,18 +5,17 @@ include "holberton.h"
  * *@dest: memory area to paste at
  * *@src: memory area to copy from
  * @n: number of bytes to copy
- * Return: a pointer to the copy me
+ * Return: a pointer to the copy
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int i = 0;
 
-	while (n > 0)
+	while (i < n)
 	{
 		dest[i] = src[i];
 		i++;
-		n--;
 	}
 	return (dest);
 }
