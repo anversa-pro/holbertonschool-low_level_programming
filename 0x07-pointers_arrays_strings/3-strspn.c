ea1f90dee2a704925dfaeeb7c6ff15d5172417d0
anversa-pro <angelavergaras@gmail.com>
2021-07-05 21:46:37 -0700

    [MODIFY] function and conditionals

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,13 +1,19 @@
-include "holberton.h"
+#include "holberton.h"
 
 /**
  * *_strspn - gets the length of a prefix substring
- * *@s: array input to search in
+ * *@s: pointer to  an array
  * *@accept: segment to look for
  * Return: number of bytes
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 	return (s);
 }
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,5 +9,17 @@ include "holberton.h"
 
 char *_strpbrk(char *s, char *accept)
 {
-	return (s);
+	int i = 0, j = 0;
+
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				return (&s[i]);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
 }
