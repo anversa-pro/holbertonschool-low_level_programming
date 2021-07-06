7179035adef8e3296fecbecdedbaba78bcf0e49f
anversa-pro <angelavergaras@gmail.com>
2021-07-05 21:17:18 -0700

    [MODIFY] function and conditional

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-include "holberton.h"
+#include "holberton.h"
 
 /**
  * *_memset - fills memory with a constant byte
@@ -10,5 +10,11 @@ include "holberton.h"
 
 char *_memset(char *s, char b, unsigned int n)
 {
+	int i = 0;
+
+	while (i < 0)
+	{
+		s[i] = b;
+	}
 	return (s);
 }
