7d28371ebb8148e45c7a951b72097ab8f02a2b69
anversa-pro <angelavergaras@gmail.com>
2021-07-06 19:46:10 -0700

    [MODIFY] change iter

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,11 +11,11 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j, k, l;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; haystack[j] != '\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[i] == haystack[j])
+			if (haystack[i] == needle[j])
 			{
 				k = i + 1;
 				l = j + 1;
