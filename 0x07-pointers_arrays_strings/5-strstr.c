5caa55da6f18306b7394847c58b916b149526a27
anversa-pro <angelavergaras@gmail.com>
2021-07-06 17:55:18 -0700

    [MODIFY] delete spaces

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,25 +7,26 @@
  * Return: a pointer to the first occurrence
  */
 
-char *_strstr(char *haystack, char *needle)                                     
-{                                                                               
-        int i;                                                                  
-        int j, k, l; 
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k, l;
 
-        for (i = 0; needle[i] != '\0'; i++)                                     
-        {                                                                       
-                for (j = 0; haystack[j] != '\0'; j++)                           
-                {                                                               
-                        if (needle[i] == haystack[j])
-                        {
-                              for (k = i + 1, l = j + 1; needle[k] != '\0'; k++, l++)
-                              {
-                                    if (needle[k] != haystack[l])
-                                        return (NULL);                                                
-                              }
-                                return (&haystack[j]);                          
-                        }
-                }                                                               
-        }                                                                       
-        return (NULL);                                                          
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (needle[i] == haystack[j])
+			{
+				k = i + 1;
+				l = j + 1;
+				for (; needle[k] != '\0'; k++, l++)
+				{
+					if (needle[k] != haystack[l])
+						return (NULL);
+				}
+				return (&haystack[j]);
+			}
+		}
+	}
+	return (NULL);
 }
