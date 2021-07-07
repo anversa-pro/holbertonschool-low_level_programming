a4b36ab7646f8fca4e61aec6274656d4a9133908
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-06 19:21:13 -0500

    [MODIFY] add conditionals

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,18 +7,25 @@
  * Return: a pointer to the first occurrence
  */
 
-char *_strstr(char *haystack, char *needle)
-{
-	int i;
-	int j;
+char *_strstr(char *haystack, char *needle)                                     
+{                                                                               
+        int i;                                                                  
+        int j, k, l; 
 
-	for (i = 0; needle[i] != '\0'; i++)
-	{
-		for (j = 0; haystack[j] != '\0'; j++)
-		{
-			if (needle[i] == haystack[j])
-				return (&haystack[j]);
-		}
-	}
-	return (NULL);
+        for (i = 0; needle[i] != '\0'; i++)                                     
+        {                                                                       
+                for (j = 0; haystack[j] != '\0'; j++)                           
+                {                                                               
+                        if (needle[i] == haystack[j])
+                        {
+                              for (k = i + 1, l = j + 1; needle[k] != '\0'; k++, l++)
+                              {
+                                    if (needle[k] != haystack[l])
+                                        return (NULL);                                                
+                              }
+                                return (&haystack[j]);                          
+                        }
+                }                                                               
+        }                                                                       
+        return (NULL);                                                          
 }
