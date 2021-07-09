781293d2d8b1e40eea8ac21d63991d4cd3b56e3c
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-08 23:38:16 -0500

    [CREATE] file & function

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the minimum number of coins to make change for a given amount
+ * @argc: argument number
+ * @argv: array of arguments
+ * Return: number of coins or 1
+ **/
+int main(int argc, char *argv[])
+{
+  return (0);
+}
