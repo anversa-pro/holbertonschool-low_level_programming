030878fe304fc458dbad620f98e7b6d98966203b
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-08 23:24:48 -0500

    [CREATE] file & function

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints its name 
+ * @argc: argument numbers
+ * @argv: array of arguments
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}
