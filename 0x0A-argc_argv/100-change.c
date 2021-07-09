944a43f1662e60c02b1bec4940b18e808f08611d
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-09 18:35:30 -0500

    [MODIFY] add conditionals and asignations

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,5 +8,39 @@
  **/
 int main(int argc, char *argv[])
 {
-  return (0);
+int number,  module, coins = 0;
+  
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+number = atoi(argv[1]);
+if (number < 0)
+printf("0\n");
+else
+{
+while (number > 0)
+{
+if(number / 25 != 0)
+module = 25;
+else if(number / 10 != 0)
+module = 10;
+else if(number / 5 != 0)
+module = 5;
+else if(number / 2 != 0)
+module = 2;
+else
+{
+coins = coins + number;
+number = 0;
+break;
+}
+coins = coins + (number / module);
+number = number % module;
+}
+}
+printf("%d\n", coins);
+return (0);
+return (0);
 }
