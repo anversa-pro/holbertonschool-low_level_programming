571759f844bee4567748514662fd9a7b83096498
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-08 23:32:10 -0500

    [CREATE] file & function

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiply two numbers
+ * @argc: arguments number
+ * @argv: array of arguements
+ * Return: result of multiplication or 1
+ **/
+int main(int argc, char *argv[])
+{
+  return(0);
+}
