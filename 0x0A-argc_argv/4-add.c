e6bb9cb5ed3cb8e307a7c836651a4761fe0c4af6
anversa-pro <angelavergaras@gmail.com>
2021-07-09 15:58:30 -0700

    [MODIFY] fix betty errors

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,24 +11,25 @@
 
 int main(int argc, char *argv[])
 {
-  int i, j, sum = 0;
-  if (argc < 2)
-    printf ("%d\n", sum);
-  else
-  {
-    for(i = 1; i < argc; i++)
-    {
-      for(j = 0; argv[i][j] != '\0'; j++)
-      {
-        if (isdigit(argv[i][j]) == 0)
-        {
-          printf("Error\n");
-          return (1);
-        }
-      }
-      sum += atoi(argv[i]);
-    }
-    printf("%d\n", sum);
-  }
-  return (0);
+	int i, j, sum = 0;
+
+	if (argc < 2)
+		printf("%d\n", sum);
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (isdigit(argv[i][j]) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
 }
