fd4fba20e3839c5bc13708607ca692aedffa9609
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-08 23:36:22 -0500

    [CREATE] file & function

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - add positive numbers
+ * @argc: argument number
+ * @argv: array of arguements
+ * Return: result of addition or 1
+ **/
+int main(int argc, char *argv[])
+{
+  return (0);
+}
