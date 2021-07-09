b78c1270c4264c245593fd45b11f78270e22f84f
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-09 17:49:58 -0500

    [MODIFY] add declarations and conditionals

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
+
 /**
  * main - add positive numbers
  * @argc: argument number
  * @argv: array of arguements
  * Return: result of addition or 1
  **/
+
 int main(int argc, char *argv[])
 {
+  int i, j, sum = 0;
+  if (argc < 2)
+    printf ("%d\n", sum);
+  else
+  {
+    for(i = 1; i < argc; i++)
+    {
+      for(j = 0; argv[i][j] != '\0'; j++)
+      {
+        if (isdigit(argv[i][j]) == 0)
+        {
+          printf("Error\n");
+          return (1);
+        }
+      }
+      sum += atoi(argv[i]);
+    }
+    printf("%d\n", sum);
+  }
   return (0);
 }
