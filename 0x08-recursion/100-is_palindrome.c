0e63a9b57d49465e784138f89cd35562465de093
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-10 17:22:45 -0500

    [CREATE] function file

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,12 @@
+include "holberton.h"
+
+/**
+ * is_palindrome - evaluates a string.
+ * *@s: string to evaluate
+ * Return: 1 if s is a palindrome or 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	return (0);
+}
