063e98c797750d6c185d91627f811bb481c5aea0
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-10 17:17:26 -0500

    [CREATE] function file

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+include "holberton.h"
+
+/**
+ * is_prime_number - Evaluates a prime number
+ * @n: input integer to evaluate
+ * Return: 1 if n is prime number 0 otherwise
+ */
+
+int is_prime_number(int n)
+{
+	return (0);
+}
