e50f3757ccc20130f95d6eece4533fb35543a579
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-10 16:33:03 -0500

    [CREATE] function file

---
+++ 2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* _strlen_recursion - returns the length of a string
+* *@s: string to process
+* Return: s length
+*/
+
+int _strlen_recursion(char *s)
+{
+  int strlength = 0;
+  return(strlength);
+}
