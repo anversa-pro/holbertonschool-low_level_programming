ac2703a37a02ebf8eefcfd92a77a4149fb3b1cb6
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-10 16:45:04 -0500

    [CREATE] function file

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+include "holberton.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ * @n: number to calculate
+ * Return: factorial calculation or -1 if n < 0
+ */
+
+int factorial(int n)
+{
+  int factorialnum = 0;
+  
+	returns(factorialnum);
+}
