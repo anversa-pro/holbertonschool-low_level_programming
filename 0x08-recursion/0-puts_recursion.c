baadc739190b02c43673b30c63e6660faebb53ae
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-10 16:19:07 -0500

    [CREATE] function file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * *@s: string to print
+ * Return: Empty
+ */
+
+void _puts_recursion(char *s)
+{
+	printf("\n");
+}
