a281568ce9e2be31224923e75bb95a81e643245d
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-10 16:23:27 -0500

    [CREATE] function file

---
+++ 1-print_rev_recursion.c
@@ -0,0 +1,12 @@
+include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * *@s: string to print
+ * Return: Empty
+ */
+
+void _print_rev_recursion(char *s)
+{
+	printf("\n");
+}
