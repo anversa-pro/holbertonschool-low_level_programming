4bb0fd253b5a462679985b5c821226401d8d0323
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-10 16:21:21 -0500

    [CREATE] main file

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+    _puts_recursion("Betty Holberton");
+    _puts_recursion("");
+    _puts_recursion("Betty \0 Holberton");
+    return (0);
+}
