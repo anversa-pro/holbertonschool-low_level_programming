a26d4d51c67e9c55d38899cd538a94f791b6a1f1
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-10 17:12:12 -0500

    [CREATE] function file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+include "holberton.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * *@n: integer to evaluate
+ * Return: calculation or -1 if there is no natural square root
+ */
+
+int _sqrt_recursion(int n)
+{
+  int squarecalc = 0;
+	return (squarecalc);
+}
