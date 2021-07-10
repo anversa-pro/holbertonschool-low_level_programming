d46941898ad103532097f85753ce029f110076e2
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-10 17:32:14 -0500

    [CREATE] function file

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,13 @@
+include "holberton.h"
+
+/**
+ * wildcmp - compares two strings
+ * *@s1: string input 1
+ * *@s2: string input 2
+ * Return: 1 if identical 0 otherwise
+ */
+  
+int wildcmp(char *s1, char *s2)
+{
+	return (0);
+}
