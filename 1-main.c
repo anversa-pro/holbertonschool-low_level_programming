8fcc9b02e69787ac0fce33290ccfb3271cbb35de
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-10 16:26:27 -0500

    [CREATE] main file

---
+++ 1-main.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+    _print_rev_recursion("\nColton Walker");
+    _print_rev_recursion("");
+    printf("\n");
+    _print_rev_recursion("Colton \0 Walker");
+    printf("\n");
+    return (0);
+}
