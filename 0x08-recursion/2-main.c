f4982d5479ce12a8690175a10321770462e97c0a
Angela Vergara <85180677+anversa-pro@users.noreply.github.com>
2021-07-10 16:40:51 -0500

    [CREATE] main file

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = _strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
+    n = _strlen_recursion("");
+    printf("%d\n", n);
+    n = _strlen_recursion("\0");
+    printf("%d\n", n);
+    return (0);
+}
