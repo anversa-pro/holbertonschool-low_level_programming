07cab88aec100fbda1a121ab0c984db8efe93935
anversa-pro <angelavergaras@gmail.com>
2021-07-13 19:23:25 -0700

    [MODIFY] add function and conditionals

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,5 +6,20 @@
 */
 char *_strdup(char *str)
 {
-	return (NULL);
+	char *s;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		continue;
+	s = malloc(i * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (j = 0; j < i; j++)
+	{
+		s[j] = str[j];
+	}
+	s[j] = '\0';
+	return (s);
 }
