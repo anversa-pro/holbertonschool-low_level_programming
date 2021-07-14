3fe7c9c50be857943a05ef470baf8e9788a919e2
anversa-pro <angelavergaras@gmail.com>
2021-07-13 20:05:18 -0700

    [MODIFY] change NULL case

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,8 +10,10 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	unsigned int i, j, k;
 
-	if (s1 == NULL || s2 == NULL)
-		return ("");
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 		continue;
 	for (j = 0; s2[j] != '\0'; j++)
