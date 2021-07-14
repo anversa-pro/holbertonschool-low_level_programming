95bd2e98d582d800ce73e2469a6e71f56c1bdf47
anversa-pro <angelavergaras@gmail.com>
2021-07-13 19:41:32 -0700

    [MODIFY] add conditionals & statements

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,5 +7,24 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-    return (NULL);
+	char *s;
+	unsigned int i, j, k, l;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		continue;
+	for (j = 0; s2[j] != '\0'; j++)
+		continue;
+	s = malloc((i + j + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (k = 0; k <= i; k++)
+	{
+		s[k] = s1[k];
+	}
+	for (k, l = 0; k <= (i + j); k++, l++)
+	{
+		s[k] = s2[l];
+	}
+	s[k] = '\0';
+	return (s);
 }
