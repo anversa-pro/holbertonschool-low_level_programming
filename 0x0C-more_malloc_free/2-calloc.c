9ba3d9ff6685dd9e713f53021632f8ae40dc35c7
anversa-pro <angelavergaras@gmail.com>
2021-07-16 22:49:06 -0700

    [MODIFY] malloc size

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,10 +12,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(nmemb * sizeof(size));
+	ptr = malloc((nmemb + 1) * sizeof(size));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb * size; i++)
+	for (i = 0; i <= nmemb * size; i++)
 		ptr[i] = 0;
 	return ((void *)ptr);
 }
