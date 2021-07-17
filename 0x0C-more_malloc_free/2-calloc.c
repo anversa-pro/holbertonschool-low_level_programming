26534594b346b427232c4e61089d4f74a4ee3487
anversa-pro <angelavergaras@gmail.com>
2021-07-16 20:47:07 -0700

    [CREATE] Function file & Conditionals

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * *_calloc - allocates memory for an array using malloc
+ * @nmemb: elements of the array
+ * @size: bytes of each element
+ * Return: pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int *ptr, i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * sizeof(size));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		ptr[i] = 0;
+	return (ptr);
+}
