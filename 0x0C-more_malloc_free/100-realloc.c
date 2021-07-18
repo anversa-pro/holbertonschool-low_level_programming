c7a235a3e1f7536c78274ec05f9bd02dec4f9017
anversa-pro <angelavergaras@gmail.com>
2021-07-17 21:54:25 -0700

    [CREATE] function file & conditionals

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,13 +2,16 @@
 /**
  * *_realloc - reallocates a memory block using malloc and free
  * *@ptr: pointer to the memory previously allocated
- * @old_size: is the size in bytes of the allocated space for ptr
- * @new_size: is the new size in bytes of the new memory block
+ * @old_size: size in bytes of the allocated space for ptr
+ * @new_size: size in bytes of the new memory block
  * Return: ptr if new == old, NULL specific cases
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	unsigned int *nptr;
+	unsigned int i;
+
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
@@ -16,5 +19,25 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-	
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size * sizeof(char *));
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	nptr = malloc(new_size * sizeof(char));
+	if (nptr == NULL)
+		return (NULL);
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			nptr[i] = ptr[i];
+		free(ptr);
+		return (nptr);
+	}
+	for (i = 0; i < new_size; i++)
+		nptr[i] = ptr[i];
+	free(ptr);
+	return (nptr);
 }
