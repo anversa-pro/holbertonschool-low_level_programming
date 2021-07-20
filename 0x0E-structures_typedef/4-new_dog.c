bf1b770d7713017c6916e62262210b6c9fc125b8
anversa-pro <angelavergaras@gmail.com>
2021-07-19 19:18:10 -0700

    [CREATE] function file & conditionals

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,44 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *new_dog - Creates a new dog copying input values
+ * *@name: Pointer to a Dog's name to copy
+ * @age: Dog's age to copy
+ * *@owner: Pointer to a Dog's owner name to copy
+ * Return: pointer to a new dog structure or NULL
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int name_l = 0, owner_l = 0, i = 0;
+	dog_t *ndog;
+
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+		return (NULL);
+	for (name_l = 0; name[name_l]; name_l++)
+		continue;
+	for (owner_l = 0; owner[owner_l]; owner_l++)
+		continue;
+	ndog->name = malloc(name_l * sizeof(char));
+	if (ndog->name == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
+	for (i = 0; i < name_l; i++)
+		ndog->name[i] = name[i];
+	ndog->age = age;
+	ndog->owner = malloc(owner_l * sizeof(char));
+	if (ndog->owner == NULL)
+	{
+		free(ndog->name);
+		free(ndog);
+		return (NULL);
+	}
+	for (i = 0; i < owner_l; i++)
+		ndog->owner[i] = owner[i];
+	return (ndog);
+}
