17477711eec845fbdff3295db92e4a3b57b88bd1
anversa-pro <angelavergaras@gmail.com>
2021-07-22 16:18:57 -0500

    [MODIFY] betty w

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,13 +5,19 @@
 /*Function that prints a name */
 void print_name(char *name, void (*f)(char *));
 
-/* Function that executes a function given as a parameter on each element of an array */
+/*
+* Function that executes a function given as a
+* parameter on each element of an array
+*/
 void array_iterator(int *array, size_t size, void (*action)(int));
 
 /* Function that searches for an integer */
 int int_index(int *array, int size, int (*cmp)(int));
 
-/* Function that selects the correct function to perform the operation asked by the user */
+/*
+* Function that selects the correct function to
+* perform the operation asked by the user
+*/
 int (*get_op_func(char *s))(int, int);
 
 /* returns the sum of a and b */
@@ -23,7 +29,7 @@ int op_sub(int a, int b);
 /* returns the product of a and b */
 int op_mul(int a, int b);
 
-/* returns the result of the division of a by b */ 
+/* returns the result of the division of a by b */
 int op_div(int a, int b);
 
 /* returns the remainder of the division of a by b */
