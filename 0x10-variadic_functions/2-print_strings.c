30fe2601dea0b8621f39d6c975f691e09659e488
anversa-pro <angelavergaras@gmail.com>
2021-07-23 23:49:31 -0500

    [CREATE] function file & conditionlas

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - prints strings separated by a given separator.
+ * *@separator: character to separate strings
+ * @n: number of strings passed to the function
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	unsigned int i;
+	char *ptr;
+
+	va_start(strings, n);
+	for (i = 0; i < n; i++)
+	{
+		ptr = va_arg(strings, char *);
+		if (ptr == NULL)
+			printf("(nil)");
+		else
+		{
+			printf("%s", ptr);
+			if (i < (n - 1) && separator != NULL)
+				printf("%s", separator);
+		}
+	}
+	va_end(strings);
+	printf("\n");
+}
