9fe2f7b6c81ef142b485b88f6cd690cd33c423aa
anversa-pro <angelavergaras@gmail.com>
2021-08-03 14:24:19 -0500

    [MODIFY] add comparisson loop to find nde

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,9 +4,17 @@
  * *get_nodeint_at_index - function that returns the nth node of a linked list
  * *@head: pointer to a linked list
  * @index: index of the node to search for.
- * Return: Node indexed ot null if it does not exist.
+ * Return: Node indexed or null if it does not exist.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	return (NULL);
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+	for (; i < index; i++)
+		head = head->next;
+	if (head == NULL)
+		return (NULL);
+	return (head);
 }
