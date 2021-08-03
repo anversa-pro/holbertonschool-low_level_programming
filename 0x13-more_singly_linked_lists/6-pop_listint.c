0086eb73252e155ed7fac9fa3d74dacf9e63a9ca
anversa-pro <angelavergaras@gmail.com>
2021-08-03 14:09:19 -0500

    [MODIFY] add asignations & temporal node to save data

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,7 +5,16 @@
  * **@head: Double pointer to a linked list
  * Return: new head nodes data or 0 if the list is empty
  */
-Prototype: int pop_listint(listint_t **head)
+int pop_listint(listint_t **head)
 {
-	return(0);
+	listint_t *tempnode;
+	int deletedData = 0;
+
+	if(*head == NULL)
+		return (0);
+	tempnode = *head;
+	deletedData = tempnode->n;
+	*head = (*head)->next;
+	free(tempnode);
+	return(deletedData);
 }
