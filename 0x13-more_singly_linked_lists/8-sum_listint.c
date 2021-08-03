4dc273966806b008f5b21ea898814e951f549245
anversa-pro <angelavergaras@gmail.com>
2021-08-03 14:55:50 -0500

    [MODIFY] Add conditionals and loops to sum the data

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,13 +1,21 @@
 #include "lists.h"
 
 /**
- * sum_listint - function that returns the sum of all the data of a linked list.
+ * sum_listint - returns the sum of all the data of a linked list.
  * *@head: pointer to the linked list
  * Return: Sum result or 0 if the list is empty
  */
-int sum_listint(listint_t *head);
+int sum_listint(listint_t *head)
 {
 	unsigned int listSum = 0;
 
+	if (head == NULL)
+		return (0);
+	while (head->next)
+	{
+		listSum += head->n;
+		head = head->next;
+	}
+	listSum += head->n;
 	return (listSum);
 }
