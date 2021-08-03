6d507909599085e870900446c35d13d7e6e6c6df
anversa-pro <angelavergaras@gmail.com>
2021-08-03 14:33:55 -0500

    [MODIFY] change location of conditional

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,8 +13,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 	for (; i < index; i++)
+	{
 		head = head->next;
-	if (head == NULL)
-		return (NULL);
+		if (head == NULL)
+			return (NULL);
+	}
 	return (head);
 }
