b97ee4b302a5eba1f3c69b648a610fe56596e371
anversa-pro <angelavergaras@gmail.com>
2021-08-03 21:03:06 -0500

    [FIX] missing semicolon

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (*head == NULL)
 	{
 		*head = newNode;
-		newNode->next = NULL
+		newNode->next = NULL;
 		return (newNode);
 	}
 
