426f77a33160f9e1c53750c700a8fbb086418603
anversa-pro <angelavergaras@gmail.com>
2021-08-05 22:33:29 -0500

    [MODIFY] Fix style warnings & add edge case

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,9 +4,11 @@
  * get_bit - function that returns the value of a bit at a given index
  * @n: number to print in binary representation
  * @index: position to find out the bit to print.
- * return: the value of the bit at index index or -1 if an error occured 
+ * Return: the value of the bit at index index or -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return ((n >> index) & 1);
+	if (n > 1)
+		return ((n >> index) & 1);
+	return (n & 1);
 }
