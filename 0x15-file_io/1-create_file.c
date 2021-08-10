ef490a2fde75111916f6bf230a4d1d31a93bf713
anversa-pro <angelavergaras@gmail.com>
2021-08-10 12:00:06 -0500

    [CREATE] function file & testing file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * create_file - Create a file object
+ * @filename: name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int op = 0, wr = 0, textsize = 0, cl = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		op = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+		if (op == -1)
+			return (-1);
+		for (; text_content[textsize]; textsize++)
+			continue;
+		wr = write(op, text_content, textsize);
+		if(wr == -1)
+			return (-1);
+		cl = close(op);
+		if (cl == -1)
+			return (-1);
+	}
+	else
+		text_content = "";
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,4 +4,7 @@
 /* Function that reads a text file and prints it to the POSIX standard output*/
 ssize_t read_textfile(const char *filename, size_t letters);
 
+/* Function that creates a file */
+int create_file(const char *filename, char *text_content);
+
 #endif /* MAIN_H*/
