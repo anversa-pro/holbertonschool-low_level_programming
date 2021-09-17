cfa1d2d5922e7d66b20557421f5ffba3b89a8529
anversa-pro <angelavergaras@gmail.com>
2021-09-16 22:18:30 -0500

    [CREATE] file that returns nodes number

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlistint - Function that retunrs the elements of a dlistint_t list.
+ * *@h: pointer to list
+ * Return: Nodes number
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodesNumber = 0;
+
+	for (; h; nodesNumber++)
+	{
+		if (!h->n)
+		{
+			h = h->next;
+			nodesNumber++;
+		}
+		h = h->next;
+	}
+	return (nodesNumber);
+}
