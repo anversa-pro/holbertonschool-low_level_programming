f6c83ec213fb03efdf39e6dc1103ddc8a08b690d
anversa-pro <angelavergaras@gmail.com>
2021-09-17 12:54:11 -0500

    [CREATE] file to add a node at begg

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * *add_dnodeint - Adds a new node at the beginning of a dlistint_t list.
+ * **@head: double pointer to head node
+ * @n: data to add at the beginning of the list
+ * Return: New Element Address or Null if it failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newElement;
+
+/*	if(head == NULL)
+		return(NULL); 
+*/
+	newElement = malloc(sizeof(dlistint_t));
+	if (newElement == NULL)
+	{
+		return (NULL);
+	}
+	newElement->n = n;
+	newElement->prev = NULL;
+	if (*head)
+	{
+		(*head)->prev = newElement;
+		newElement->next = *head;
+	}
+	else
+	{
+		newElement->next = NULL;
+	}
+	*head = newElement;
+	return (newElement);
+}
