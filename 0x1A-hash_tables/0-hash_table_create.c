dbe889e6b5892f80dc607c69d72f370ac43ba80b
anversa-pro <angelavergaras@gmail.com>
2021-10-09 13:01:01 -0500

    [MODIFY] change to calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_hash_table->size = size;
-	new_hash_table->array = malloc(sizeof(hash_node_t) * size);
+	new_hash_table->array = calloc(size, sizeof(hash_node_t *));
 	if (new_hash_table->array == NULL)
 	{
 		free(new_hash_table);
